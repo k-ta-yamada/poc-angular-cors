219b53fa6cbb2ae7744d1cf8a728a28cf71a9759
k-ta-yamada <key.luvless@gmail.com>
2022-11-23 22:14:33 +0900

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'poc-angular-cors';
+export class AppComponent implements OnInit {
+
+  json: Object | undefined;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    // const url = 'https://k-ta-yamada.me/api/plank';
+    const url = 'https://api.github.com/repos/k-ta-yamada/k-ta-yamada/branches';
+    this.http.get(url).subscribe(x => {
+      console.log(x);
+      this.json = x;
+    });
+  }
+
 }
